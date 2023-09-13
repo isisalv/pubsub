b847bef5521067eaf297a0a7507d9816ea6d391d
Isis <isis_rodrigues@hotmail.com>
2023-09-12 22:40:04 -0300

    DONE: ajustes

--- pub.py
+++ pub.py
@@ -1,27 +1,30 @@
-import pika
+import pika, sys, os
 
 TIPOS_EVENTOS = ['workshop', 'palestra', 'amostra']
 
-def envia_mensagem(mensagem, topico):
-    # Conexão
+def envia_mensagem(mensagem:str, topico:str):
+    # Estabelece conexão com o message broker
     connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
     channel = connection.channel()
 
     # Declara tópico
-    channel.exchange_declare(exchange='logs', exchange_type='topic')
+    channel.exchange_declare(exchange='eventos', exchange_type='topic')
 
     # Envia mensagem
-    channel.basic_publish(exchange='logs',routing_key=topico, body=mensagem)
-    print(f"Mensagem enviada - {topico}: {mensagem}")
+    channel.basic_publish(exchange='eventos',routing_key=topico, body=mensagem)
 
     # Fecha conexão
     connection.close()
 
 # Enviar mensagens
 while(True):
+    publicador = sys.argv[1].upper() if len(sys.argv) > 1 else sys.argv[0]
+    os.system('cls')
+    print(f"\nPUBLICADOR - {publicador}")
+    print("Eu envio divulgações de eventos!!\n\n")
     print("Selecione um tipo de evento: \n")
     for i, evento in enumerate(TIPOS_EVENTOS):
-        print(f"{i+1} - {evento}")
+        print(f"{i+1} - {evento.capitalize()}")
     opt = int(input()) - 1
     mensagem = input("O nome do evento: ")
     envia_mensagem(mensagem, TIPOS_EVENTOS[opt])
--- sub.py
+++ sub.py
@@ -1,11 +1,11 @@
-import pika
+import pika, sys, os
 
 TIPOS_EVENTOS = ['workshop', 'palestra', 'amostra']
 
 
 def callback(ch, method, properties, body):
-    topico = method.routing_key
-    print(f"Um novo evento pra você!  {topico}: {body}")
+    topico = method.routing_key.capitalize()
+    print(f"\nUm novo evento pra você!  {topico}: {body.decode()}")
 
 def recebe_mensagens(topicos):
     # Conexão
@@ -14,15 +14,15 @@ def recebe_mensagens(topicos):
     channel = connection.channel()
 
     # Declara tópico
-    channel.exchange_declare(exchange='logs', exchange_type='topic')
+    channel.exchange_declare(exchange='eventos', exchange_type='topic')
 
     result = channel.queue_declare(queue='', exclusive=True)
     queue_name = result.method.queue
 
     for topico in topicos:
-        channel.queue_bind(exchange='logs', queue=queue_name, routing_key=topico)
+        channel.queue_bind(exchange='eventos', queue=queue_name, routing_key=topico)
 
-    print('Recebendo notificação de eventos =)')
+    print('\nRecebendo notificações de eventos =)')
 
     channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
 
@@ -33,18 +33,24 @@ continuar = True
 
 # Selecionar tópicos pra inscrever
 while(continuar):
+    assintante = sys.argv[1].upper() if len(sys.argv) > 1 else sys.argv[0]
+    os.system('cls')
+    print(f"\nASSINANTE - {assintante}")
+    print("Eu recebo divulgações de eventos!!\n\n")
     print("Selecione um tipo de evento para ser notificado: \n")
     for i, evento in enumerate(TIPOS_EVENTOS):
-        print(f"{i+1} - {evento}")
+        print(f"{i+1} - {evento.capitalize()}")
     opt = int(input()) - 1
     topico = TIPOS_EVENTOS[opt]
     if topico not in subscritos:
         subscritos.append(TIPOS_EVENTOS[opt])
-        opt = input("Deseja adicionar outro tipo de evento? (s/n)")
+        opt = input("Deseja adicionar outro tipo de evento? (s/n) ")
         if opt != 's':
             continuar = False
+            os.system('cls')
     else:
         print("Você já escolheu esse tipo de evento!")
+        input()
 
-# Inicia consumo
+# Inicia consumo de mensagens
 recebe_mensagens(subscritos)
